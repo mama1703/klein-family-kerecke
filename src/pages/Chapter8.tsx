4eedf802315def070163d1488780b56b623163c0
mama1703 <yifat.a.danieli@gmail.com>
2026-04-21 19:44:50 +0300

    Update Chapter8.tsx

--- src/pages/Chapter8.tsx
+++ src/pages/Chapter8.tsx
@@ -1,29 +1,26 @@
 import { ChapterBottomNav } from "../components/ChapterBottomNav";
-
 import { ImageBlock } from "../components/ImageBlock";
+import { StoryNav } from "../components/StoryNav";
 
 export default function Chapter8() {
   return (
     <div className="prose-hebrew">
       <h2 className="text-4xl font-bold mb-10 text-center">
-נספח - תמונות ממפגש הנוער הציוני בארץ
+        נספח - תמונות ממפגש הנוער הציוני בארץ
       </h2>
 
-      {/* תמונה */}
       <ImageBlock
         src="/klein-family-kerecke/images/with-yona.png"
         alt="מפגש עליית הנוער 1998"
         caption={'מפגש חברי עליית הנוער, 1998 - חיים ואווי קליין ובאמצע יונה אמתי (אילוב)'}
       />
 
-      {/* תמונה */}
       <ImageBlock
         src="/klein-family-kerecke/images/with-shayo.png"
         alt="מפגש עליית הנוער עם שעיו"
         caption={'משמאל לימין: מאיר לדרמן, חיים קליין, שעיו (מאחור), פיש ארנה, קאין מאיר וליסטמן'}
       />
 
-      {/* תמונה */}
       <ImageBlock
         src="/klein-family-kerecke/images/with-yehudit.png"
         alt="יהודית ריצר דוורית המחתרת"
@@ -31,6 +28,10 @@ export default function Chapter8() {
       />
 
       <ChapterBottomNav />
+
+      <StoryNav
+        next={{ to: "/story-hershi", title: "הסיפור של הרשי", icon: "book" }}
+      />
     </div>
   );
 }
